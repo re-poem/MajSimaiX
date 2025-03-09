f179c7a8fa1830e6a5c3777509c700a22c37829e
LeZi9916 <lezi@leziblog.com>
2025-03-09 15:22:22 +0800

    feat: InvalidSimaiSyntaxException

---
+++ Runtime/Exceptions/InvalidSimaiSyntaxException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MajSimai
+{
+    public class InvalidSimaiSyntaxException : InvalidSimaiMarkupException
+    {
+        public InvalidSimaiSyntaxException(int line, int column, string content) : base(line,column,content)
+        {
+
+        }
+        public InvalidSimaiSyntaxException(int line, int column, string content, string message) : base(line,column,content,message)
+        {
+
+        }
+    }
+}
