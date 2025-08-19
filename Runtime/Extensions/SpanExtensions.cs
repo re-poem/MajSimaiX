303277a328e045fb61bfa32834ddc5e9fdbc6c2e
LeZi9916 <lezi@leziblog.com>
2025-08-19 14:44:01 +0800

    feat: ReadOnlySpan<T>.Replace feat: Span<T>.Replace chore: allow unsafe code chore: add nuget package "System.Runtime.CompilerServices.Unsafe"

--- Runtime/Extensions/SpanExtensions.cs
+++ Runtime/Extensions/SpanExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace MajSimai
@@ -78,6 +79,62 @@ namespace MajSimai
         {
             return span.Length != 0 && (span[0]?.Equals(value) ?? (object?)value is null);
         }
+        public static void Replace<T>(this Span<T> source, T oldValue, T newValue) where T : IEquatable<T>?
+        {
+            if (source.IsEmpty)
+            {
+                return;
+            }
+            for (var i = 0; i < source.Length; i++)
+            {
+                ref var value = ref source[i];
+                if ((value?.Equals(value) ?? oldValue is null))
+                {
+                    value = newValue;
+                }
+            }
+        }
+        /// <summary>
+        /// Copies <paramref name="source"/> to <paramref name="dest"/>, replacing all occurrences of <paramref name="oldValue"/> with <paramref name="newValue"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the spans.</typeparam>
+        /// <param name="source">The span to copy.</param>
+        /// <param name="dest">The span into which the copied and replaced values should be written.</param>
+        /// <param name="oldValue">The value to be replaced with <paramref name="newValue"/>.</param>
+        /// <param name="newValue">The value to replace all occurrences of <paramref name="oldValue"/>.</param>
+        /// <exception cref="ArgumentException">The <paramref name="dest"/> span was shorter than the <paramref name="source"/> span.</exception>
+        /// <exception cref="ArgumentException">The <paramref name="source"/> and <paramref name="dest"/> were overlapping but not referring to the same starting location.</exception>
+        public static unsafe void Replace<T>(this ReadOnlySpan<T> source, Span<T> dest, T oldValue, T newValue) where T : IEquatable<T>?
+        {
+#pragma warning disable CS8500
+            if (dest.Length < source.Length)
+            {
+                throw new ArgumentException("Destination is too short", nameof(dest));
+            }
+            else if(source.IsEmpty)
+            {
+                return;
+            }
+            var srcPtr = Unsafe.AsPointer(ref MemoryMarshal.GetReference(source));
+            var dstPtr = Unsafe.AsPointer(ref MemoryMarshal.GetReference(dest));
+            var offset = (ulong)(((T*)dstPtr - (T*)srcPtr) * sizeof(T));
+            var srcLen = (ulong)(source.Length * sizeof(T));
+            var dstLen = (ulong)(dest.Length * sizeof(T));
             
+            if(offset != 0 && (offset < srcLen || ulong.MinValue - offset < dstLen))
+            {
+                throw new ArgumentException("The source and dest were overlapping but not referring to the same starting location");
+            }
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                var value = source[i];
+                if ((value?.Equals(value) ?? oldValue is null))
+                {
+                    dest[i] = newValue;
+                }
+            }
+#pragma warning restore CS8500
+        }
     }
 }
